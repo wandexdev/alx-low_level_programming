846129469fd8db8f2394468832da95a0b419b90e
wandexdev <tiwande04@gmail.com>
2022-05-26 15:20:40 +0100

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,8 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint -
- * Desc- prints all the elements of a list.
+ * print_listint - Descp: prints all the elements of a list.
  * @h: head of a list.
  *
  * Return: numbers of nodes.
