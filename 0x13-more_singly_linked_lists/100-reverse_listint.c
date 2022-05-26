e4655c39d62d1ca5467164ed34329fc90cf4932f
wandexdev <tiwande04@gmail.com>
2022-05-26 17:17:41 +0100

    AdvT1

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,47 +1,27 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index -Descp- deletes the node at index
- * of a linked list.
+ * reverse_listint -Descp- reverses a linked list.
  * @head: head of a list.
- * @index: index of the list where the node is
- * deleted.
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * Return: pointer to the first node.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *prev;
-	listint_t *next;
+	listint_t *p;
+	listint_t *n;
 
-	prev = *head;
+	p = NULL;
+	n = NULL;
 
-	if (index != 0)
+	while (*head != NULL)
 	{
-		for (i = 0; i < index - 1 && prev != NULL; i++)
-		{
-			prev = prev->next;
-		}
+		n = (*head)->next;
+		(*head)->next = p;
+		p = *head;
+		*head = n;
 	}
 
-	if (prev == NULL || (prev->next == NULL && index != 0))
-	{
-		return (-1);
-	}
-
-	next = prev->next;
-
-	if (index != 0)
-	{
-		prev->next = next->next;
-		free(next);
-	}
-	else
-	{
-		free(prev);
-		*head = next;
-	}
-
-	return (1);
+	*head = p;
+	return (*head);
 }
